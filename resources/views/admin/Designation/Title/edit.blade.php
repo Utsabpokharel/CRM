2185a72c42509a03cd907a1a5af113389c91d3ac
unknown <pokharelutsab567@gmail.com>
2020-12-02 17:54:52 +0545

    title CRUD and validation

--- resources/views/admin/Designation/Title/edit.blade.php
+++ resources/views/admin/Designation/Title/edit.blade.php
@@ -1,13 +1,14 @@
 @extends('admin.layouts.default')
-@section('page_title','Add Title')
+@section('page_title','Edit Title')
 @section('content')
 <div class="card-body card">
-    <form class="form" action="{{route('title.store')}}" method="post" autocomplete="on">
+    <form class="form" action="{{route('title.update',$title->id)}}" method="post" autocomplete="on">
         @csrf
+        @method('put')
         <div class="form-group">
             <label>Title Name:</label>
             <input type="text" class="form-control form-control-solid @error('title') is-invalid @enderror"
-            name="title" placeholder="Enter Title Name" value="{{old('title','')}}" />
+            name="title" placeholder="Enter Title Name" value="{{old('title',$title->title)}}" />
             @error('title')
                 <span class="invalid-feedback" role="alert">{{$message}}</span>
             @enderror
@@ -15,7 +16,7 @@
         <div class="form-group">
             <label>Description:</label>
             <textarea name="description" id="" cols="30" rows="10" class="form-control form-control-solid @error('description') is-invalid @enderror"
-            placeholder="Enter description here" >{{old('description','')}}</textarea>
+            placeholder="Enter description here" >{{old('description',$title->description)}}</textarea>
             @error('description')
                 <span class="invalid-feedback" role="alert">{{$message}}</span>
             @enderror
