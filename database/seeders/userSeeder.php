3af2c5f64e8808f4f2222daf09ac1d2ecfe4a2d3
Utsabpokharel <pokharelutsab567@gmail.com>
2020-12-10 12:37:55 +0545

    seeder changes

--- database/seeders/userSeeder.php
+++ database/seeders/userSeeder.php
@@ -16,13 +16,51 @@ class userSeeder extends Seeder
     {
 
         $user = array(
-            'name' => 'admin',
-            'email' => 'admin@admin.com',
-            'phone'=>'123456789',
-            'password' => bcrypt('admin123'),
-            'gender'=>'male',
-            'address' => 'Kathmandu',
-            'roleid'=>1
+                array(
+                    'name' => 'Super Admin',
+                    'email' => 'super@admin.com',
+                    'phone'=>'123456789',
+                    'password' => bcrypt('super123'),
+                    'gender'=>'male',
+                    'address' => 'Kathmandu',
+                    'roleid'=>1
+                ),
+                array(
+                    'name' => 'Admin',
+                    'email' => 'admin@admin.com',
+                    'phone'=>'123456789',
+                    'password' => bcrypt('admin123'),
+                    'gender'=>'male',
+                    'address' => 'Kathmandu',
+                    'roleid'=>2
+                ),
+                array(
+                    'name' => 'Staff',
+                    'email' => 'staff@staff.com',
+                    'phone'=>'123456789',
+                    'password' => bcrypt('staff123'),
+                    'gender'=>'male',
+                    'address' => 'Kathmandu',
+                    'roleid'=>3
+                ),
+                array(
+                    'name' => 'Customer',
+                    'email' => 'customer@customer.com',
+                    'phone'=>'123456789',
+                    'password' => bcrypt('customer123'),
+                    'gender'=>'male',
+                    'address' => 'Kathmandu',
+                    'roleid'=>4
+                ),
+                array(
+                    'name' => 'Vendor',
+                    'email' => 'vendor@vendor.com',
+                    'phone'=>'123456789',
+                    'password' => bcrypt('vendor123'),
+                    'gender'=>'male',
+                    'address' => 'Kathmandu',
+                    'roleid'=>5
+                ),
         );
         DB::table('users')->insert($user);
     }
