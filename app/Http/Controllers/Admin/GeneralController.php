7b76d3ac9d0ea69b97103861d906587012caba94
Utsabpokharel <pokharelutsab567@gmail.com>
2020-12-21 13:39:00 +0545

    notifications

--- app/Http/Controllers/Admin/GeneralController.php
+++ app/Http/Controllers/Admin/GeneralController.php
@@ -15,24 +15,24 @@ class GeneralController extends Controller
     }
      public function store(Request $request)
     {
-        $request->validate([
-            'website_name' => 'required|min:3|max:20|alpha',
-            'website' => 'required|min:3|max:20|alpha',
-            'email' => 'required'
-            
-        ],[
-            'website_name.required'=>'Website Name is required',
-            'website_name.min'=>'The Website Name must be at least 3 characters.',
-            'website_name.max'=>'The Website Name Name may not be greater than 20 characters.',
-            
-            'email.required'=>'Email is required'
-           
+        // $request->validate([
+        //     'website_name' => 'required|min:3|max:20|alpha',
+        //     'website' => 'required|min:3|max:20|alpha',
+        //     'email' => 'required'
 
-             ]);
+        // ],[
+        //     'website_name.required'=>'Website Name is required',
+        //     'website_name.min'=>'The Website Name must be at least 3 characters.',
+        //     'website_name.max'=>'The Website Name Name may not be greater than 20 characters.',
+
+        //     'email.required'=>'Email is required'
+
+
+        //      ]);
         $data = $request->all();
 
         $data['website_logo'] = save_image($request->website_logo, 150, 150, $this->imagePath());
-        
+
 
         General::create($data);
         return redirect()->route('general.create');
