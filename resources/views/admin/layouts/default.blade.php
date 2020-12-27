fa223ee612191f43fa597949d02277dcf51bbca0
sanjog karki <sanjogkarki1460>
2020-12-27 18:05:03 +0545

    real time notification done

--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -3,245 +3,269 @@
 <!--begin::Head-->
 
 <head>
-	<base href="">
-    <meta charset="utf-8" />
+    <base href="">
+    <meta charset="utf-8"/>
     {{-- {{dd(Auth::user()->general)}} --}}
     @if(Auth::user()->general !=[])
         <title>{{Auth::user()->general['website_name']}} | @section('page_title') @show</title>
     @else
         <title>CRM| @section('page_title') @show</title>
     @endif
-	<meta name="description" content="Metronic admin dashboard live demo. Check out all the features of the admin panel. A large number of settings, additional services and widgets." />
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-	<link rel="canonical" href="https://keenthemes.com/metronic" />
-	<!--begin::Fonts-->
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
-	<!--end::Fonts-->
-	<!--begin::Page Vendors Styles(used by this page)-->
-	<link href="{{asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.css')}}" rel="stylesheet" type="text/css" />
-	<!--end::Page Vendors Styles-->
-	<!--begin::Global Theme Styles(used by all pages)-->
-	<link href="{{asset('assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
-	<link href="{{asset('assets/plugins/custom/prismjs/prismjs.bundle.css')}}" rel="stylesheet" type="text/css" />
-	<link href="{{asset('assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
-	<link rel="stylesheet" type="text/css" href="{{asset('assets/js/pages/crud/datatables/css/jquery.dataTables.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('assets/js/pages/crud/datatables/css/responsive.dataTables.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css">
-	<!--end::Global Theme Styles-->
-	<!--begin::Layout Themes(used by all pages)-->
-	<link href="{{asset('assets/css/themes/layout/header/base/light.css')}}" rel="stylesheet" type="text/css" />
-	<link href="{{asset('assets/css/themes/layout/header/menu/light.css')}}" rel="stylesheet" type="text/css" />
-	<link href="{{asset('assets/css/themes/layout/brand/dark.css')}}" rel="stylesheet" type="text/css" />
-	<link href="{{asset('assets/css/themes/layout/aside/dark.css')}}" rel="stylesheet" type="text/css" />
-	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/sweetalert.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('assets/css/my.css')}}">
+    <meta name="description"
+          content="Metronic admin dashboard live demo. Check out all the features of the admin panel. A large number of settings, additional services and widgets."/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <link rel="canonical" href="https://keenthemes.com/metronic"/>
+    <!--begin::Fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"/>
+    <!--end::Fonts-->
+    <!--begin::Page Vendors Styles(used by this page)-->
+    <link href="{{asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.css')}}" rel="stylesheet"
+          type="text/css"/>
+    <!--end::Page Vendors Styles-->
+    <!--begin::Global Theme Styles(used by all pages)-->
+    <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/plugins/global/plugins.bundle.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/plugins/custom/prismjs/prismjs.bundle.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/css/style.bundle.css')}}" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" type="text/css"
+          href="{{asset('assets/js/pages/crud/datatables/css/jquery.dataTables.min.css')}}">
+    <link rel="stylesheet" type="text/css"
+          href="{{asset('assets/js/pages/crud/datatables/css/responsive.dataTables.min.css')}}">
+    <link rel="stylesheet" type="text/css"
+          href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css">
+    <!--end::Global Theme Styles-->
+    <!--begin::Layout Themes(used by all pages)-->
+    <link href="{{asset('assets/css/themes/layout/header/base/light.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/css/themes/layout/header/menu/light.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/css/themes/layout/brand/dark.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/css/themes/layout/aside/dark.css')}}" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/sweetalert.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/my.css')}}">
 
-	<!--end::Layout Themes-->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+
+
+    <!--end::Layout Themes-->
     @if(Auth::user()->general !=[])
-    <link rel="shortcut icon" href="{{asset('images/general/'.Auth::user()->general['website_logo'])}}" />
+        <link rel="shortcut icon" href="{{asset('images/general/'.Auth::user()->general['website_logo'])}}"/>
     @else
-    <link rel="shortcut icon" href="{{asset('assets/media/logos/logo-2.png')}}" />
+        <link rel="shortcut icon" href="{{asset('assets/media/logos/logo-2.png')}}"/>
     @endif
 
 </head>
 <!-- Data Tables -->
 
 
-
 @stack('css')
 <style type="text/css">
-	.swal2-icon.swal2-success {
-		border-color: #a5dc86;
-		position: relative;
-		left: 178px;
-	}
+    .swal2-icon.swal2-success {
+        border-color: #a5dc86;
+        position: relative;
+        left: 178px;
+    }
 
-	.swal2-icon.swal2-warning {
-		border-color: #a5dc86;
-		position: relative;
-		left: 178px;
-	}
+    .swal2-icon.swal2-warning {
+        border-color: #a5dc86;
+        position: relative;
+        left: 178px;
+    }
 
-	li.flex {
-		flex: 1s;
-	}
+    li.flex {
+        flex: 1s;
+    }
 
-	tr.dtrg-group.dtrg-start.dtrg-level-0 {
-		display: none;
-	}
+    tr.dtrg-group.dtrg-start.dtrg-level-0 {
+        display: none;
+    }
 
-	.table-bg {
-		background: #1a1a27;
-	}
+    .table-bg {
+        background: #1a1a27;
+    }
 
-	thead.table-bg th {
-		color: #fff !important;
-	}
+    thead.table-bg th {
+        color: #fff !important;
+    }
 
-	.dataTables_wrapper .dataTables_paginate .paginate_button.current,
-	.dataTables_wrapper .dataTables_paginate .paginate_button.current:hover {
-		color: #fff !important;
-		border: 1px solid #3699FF !important;
-		background: linear-gradient(to bottom, #3699FF 0%, #3699FF 100%) !important;
-	}
+    .dataTables_wrapper .dataTables_paginate .paginate_button.current,
+    .dataTables_wrapper .dataTables_paginate .paginate_button.current:hover {
+        color: #fff !important;
+        border: 1px solid #3699FF !important;
+        background: linear-gradient(to bottom, #3699FF 0%, #3699FF 100%) !important;
+    }
 
-	.img-css {
-		width: 40px;
-		height: 40px;
-		border-radius: 50%;
+    .img-css {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+    }
 
-	}
+    .notification-counter {
+        position: absolute;
+        top: -2px;
+        right: -5px;
+        padding: 5px 10px;
+        border-radius: 50%;
+        color: white;
+    }
 
-	.notification-counter{
-		position: absolute;
-		top: -2px;
-		right: -5px;
-		padding: 5px 10px;
-		border-radius: 50%;
-		color: white;
-	}
+    .footercss{
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        text-align: center;
+    }
+
 </style>
 </head>
 
 <!--end::Head-->
 <!--begin::Body-->
 
-<body id="kt_body" class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading">
-	<!--begin::Main-->
-	<!--begin::Header Mobile-->
-	@include('admin.layouts.headermobile')
-	<!--end::Header Mobile-->
-	<div class="d-flex flex-column flex-root">
-		<!--begin::Page-->
-		<div class="d-flex flex-row flex-column-fluid page">
-			<!--begin::Aside-->
-			@include('admin.layouts.sidebar')
-			<!--end::Aside-->
-			<!--begin::Wrapper-->
-			<div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
-				<!--begin::Header-->
-				@include('admin.layouts.header')
-				<!--end::Header-->
-				<!--begin::Content-->
-				<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
-					<!--begin::Subheader-->
-					<div class="subheader py-2 py-lg-4 subheader-solid" id="kt_subheader">
-						<div class="container-fluid d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
-							<!--begin::Info-->
-							<div class="d-flex align-items-center flex-wrap mr-2">
-								<!--begin::Page Title-->
-								<h5 class="text-dark font-weight-bold mt-2 mb-2 mr-5">@section('page_title') @show</h5>
-								<!--end::Page Title-->
-							</div>
-							<!--end::Info-->
+<body id="kt_body "
+      class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading">
+<div id="app">
+    <!--begin::Main-->
+    <!--begin::Header Mobile-->
+@include('admin.layouts.headermobile')
+<!--end::Header Mobile-->
+    <div class="d-flex flex-column flex-root">
+        <!--begin::Page-->
+        <div class="d-flex flex-row flex-column-fluid page">
+            <!--begin::Aside-->
+        @include('admin.layouts.sidebar')
+        <!--end::Aside-->
+            <!--begin::Wrapper-->
+            <div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
+                <!--begin::Header-->
+            @include('admin.layouts.header')
+            <!--end::Header-->
+                <!--begin::Content-->
+                <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+                    <!--begin::Subheader-->
+                    <div class="subheader py-2 py-lg-4 subheader-solid" id="kt_subheader">
+                        <div class="container-fluid d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
+                            <!--begin::Info-->
+                            <div class="d-flex align-items-center flex-wrap mr-2">
+                                <!--begin::Page Title-->
+                                <h5 class="text-dark font-weight-bold mt-2 mb-2 mr-5">@section('page_title') @show</h5>
+                                <!--end::Page Title-->
+                            </div>
+                            <!--end::Info-->
 
-						</div>
-					</div>
-					<!--end::Subheader-->
-					<!--begin::Entry-->
-					<div class="d-flex flex-column-fluid">
-						<!--begin::Container-->
-						<div class="container">
-							@yield('content')
-						</div>
-						<!--end::Container-->
-					</div>
-					<!--end::Entry-->
-				</div>
-				<!--end::Content-->
-				<!--begin::Footer-->
-				@include('admin.layouts.footer')
-				<!--end::Footer-->
-			</div>
-			<!--end::Wrapper-->
-		</div>
-		<!--end::Page-->
-	</div>
-	<!--end::Main-->
-	@include('admin.layouts.panels')
-	<!-- <script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script> -->
-	<!--begin::Global Config(global config for global JS scripts)-->
-	<script>
-		var KTAppSettings = {
-			"breakpoints": {
-				"sm": 576,
-				"md": 768,
-				"lg": 992,
-				"xl": 1200,
-				"xxl": 1400
-			},
-			"colors": {
-				"theme": {
-					"base": {
-						"white": "#ffffff",
-						"primary": "#3699FF",
-						"secondary": "#E5EAEE",
-						"success": "#1BC5BD",
-						"info": "#8950FC",
-						"warning": "#FFA800",
-						"danger": "#F64E60",
-						"light": "#E4E6EF",
-						"dark": "#181C32"
-					},
-					"light": {
-						"white": "#ffffff",
-						"primary": "#E1F0FF",
-						"secondary": "#EBEDF3",
-						"success": "#C9F7F5",
-						"info": "#EEE5FF",
-						"warning": "#FFF4DE",
-						"danger": "#FFE2E5",
-						"light": "#F3F6F9",
-						"dark": "#D6D6E0"
-					},
-					"inverse": {
-						"white": "#ffffff",
-						"primary": "#ffffff",
-						"secondary": "#3F4254",
-						"success": "#ffffff",
-						"info": "#ffffff",
-						"warning": "#ffffff",
-						"danger": "#ffffff",
-						"light": "#464E5F",
-						"dark": "#ffffff"
-					}
-				},
-				"gray": {
-					"gray-100": "#F3F6F9",
-					"gray-200": "#EBEDF3",
-					"gray-300": "#E4E6EF",
-					"gray-400": "#D1D3E0",
-					"gray-500": "#B5B5C3",
-					"gray-600": "#7E8299",
-					"gray-700": "#5E6278",
-					"gray-800": "#3F4254",
-					"gray-900": "#181C32"
-				}
-			},
-			"font-family": "Poppins"
-		};
-	</script>
-	<!--end::Global Config-->
-	<!--begin::Global Theme Bundle(used by all pages)-->
-	<script src="{{asset('assets/plugins/global/plugins.bundle.js')}}"></script>
-	<script src="{{asset('assets/plugins/custom/prismjs/prismjs.bundle.js')}}"></script>
-	<script src="{{asset('assets/js/scripts.bundle.js')}}"></script>
-	<script src="{{asset('assets/js/pages/crud/datatables/js/jquery.dataTables.min.js')}}"></script>
-	<script src="{{asset('assets/js/pages/crud/datatables/js/dataTables.responsive.min.js')}}"></script>
-	<!--end::Global Theme Bundle-->
-	<!--begin::Page Vendors(used by this page)-->
-	<script src="{{asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.js')}}"></script>
-	<!--end::Page Vendors-->
-	<!--begin::Page Scripts(used by this page)-->
+                        </div>
+                    </div>
+                    <!--end::Subheader-->
+                    <!--begin::Entry-->
+                    <div class="d-flex flex-column-fluid">
+                        <!--begin::Container-->
+                        <div class="container">
+                            @yield('content')
+                        </div>
+                        <!--end::Container-->
+                    </div>
+                    <!--end::Entry-->
+                </div>
+                <!--end::Content-->
+                <!--begin::Footer-->
+            @include('admin.layouts.footer')
+            <!--end::Footer-->
+            </div>
+            <!--end::Wrapper-->
+        </div>
+        <!--end::Page-->
+    </div>
+</div>
+<!--end::Main-->
+@include('admin.layouts.panels')
+<!-- <script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script> -->
+<!--begin::Global Config(global config for global JS scripts)-->
+<script>
+    var KTAppSettings = {
+        "breakpoints": {
+            "sm": 576,
+            "md": 768,
+            "lg": 992,
+            "xl": 1200,
+            "xxl": 1400
+        },
+        "colors": {
+            "theme": {
+                "base": {
+                    "white": "#ffffff",
+                    "primary": "#3699FF",
+                    "secondary": "#E5EAEE",
+                    "success": "#1BC5BD",
+                    "info": "#8950FC",
+                    "warning": "#FFA800",
+                    "danger": "#F64E60",
+                    "light": "#E4E6EF",
+                    "dark": "#181C32"
+                },
+                "light": {
+                    "white": "#ffffff",
+                    "primary": "#E1F0FF",
+                    "secondary": "#EBEDF3",
+                    "success": "#C9F7F5",
+                    "info": "#EEE5FF",
+                    "warning": "#FFF4DE",
+                    "danger": "#FFE2E5",
+                    "light": "#F3F6F9",
+                    "dark": "#D6D6E0"
+                },
+                "inverse": {
+                    "white": "#ffffff",
+                    "primary": "#ffffff",
+                    "secondary": "#3F4254",
+                    "success": "#ffffff",
+                    "info": "#ffffff",
+                    "warning": "#ffffff",
+                    "danger": "#ffffff",
+                    "light": "#464E5F",
+                    "dark": "#ffffff"
+                }
+            },
+            "gray": {
+                "gray-100": "#F3F6F9",
+                "gray-200": "#EBEDF3",
+                "gray-300": "#E4E6EF",
+                "gray-400": "#D1D3E0",
+                "gray-500": "#B5B5C3",
+                "gray-600": "#7E8299",
+                "gray-700": "#5E6278",
+                "gray-800": "#3F4254",
+                "gray-900": "#181C32"
+            }
+        },
+        "font-family": "Poppins"
+    };
+</script>
+<!--end::Global Config-->
+<!--begin::Global Theme Bundle(used by all pages)-->
 
-	<script src="{{asset('assets/js/pages/widgets.js')}}"></script>
-	<script src="{{asset('assets/js/pages/widgets.js')}}"></script>
-	<script src="{{asset('assets/js/sweetalert.min.js')}}"></script>
-	@include('admin.layouts.messages')
-	<!--end::Page Scripts-->
-	@stack('scripts')
-	<script src="{{asset('assets/js/pages/crud/forms/validation/form-controls.js')}}"></script>
-	@yield('script')
+<script src="{{asset('js/app.js')}}"></script>
+<script src="{{asset('assets/plugins/global/plugins.bundle.js')}}"></script>
+<script src="{{asset('assets/plugins/custom/prismjs/prismjs.bundle.js')}}"></script>
+<script src="{{asset('assets/js/scripts.bundle.js')}}"></script>
+<script src="{{asset('assets/js/pages/crud/datatables/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('assets/js/pages/crud/datatables/js/dataTables.responsive.min.js')}}"></script>
+<!--end::Global Theme Bundle-->
+<!--begin::Page Vendors(used by this page)-->
+<script src="{{asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.js')}}"></script>
+<!--end::Page Vendors-->
+<!--begin::Page Scripts(used by this page)-->
+
+<script src="{{asset('assets/js/pages/widgets.js')}}"></script>
+<script src="{{asset('assets/js/pages/widgets.js')}}"></script>
+<script src="{{asset('assets/js/sweetalert.min.js')}}"></script>
+@include('admin.layouts.messages')
+<!--end::Page Scripts-->
+@stack('scripts')
+<script src="{{asset('assets/js/pages/crud/forms/validation/form-controls.js')}}"></script>
+@yield('script')
+
 </body>
 <!--end::Body-->
 
